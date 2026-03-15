092fa928bb2cc14ca9fdedd6be4cf7da55271eb2
felipeAmador7 <juanfelipeamadormunoz@gmail.com>
2026-03-15 14:54:03 -0500

    Refactor ArrayList to Stack and create Queue, CircularList

--- codigo.py
+++ codigo.py
@@ -1,236 +1,189 @@
 
-class ArrayList:
+// Código Stack
 
-    def __init__(self):
-        self._data = []
-        self._size = 0
+class Stack:
+    def _init_(self):
+        self.items = []
 
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if self.is_empty():
+            print("La pila está vacía")
+            return None
+        return self.items.pop()
+
+    
+    def peek(self):
+        if self.is_empty():
+            return None
+        return self.items[-1]
+
+    
     def size(self):
-        return self._size
+        return len(self.items)
 
+
+// Código Queque
+
+class Queue:
+    def _init_(self, initial_elements=None):
+        if initial_elements is None:
+            initial_elements = []
+        self.elements = list(initial_elements)
+    
+    def _str_(self):
+        return f"Queue({self.elements})"
+    
+    def _len_(self):
+        return len(self.elements)
+    
     def isEmpty(self):
-        return self._size == 0
-
+        return len(self.elements) == 0
     
-    def add(self, *args):
-
-        
-        if len(args) == 1:
-            element = args[0]
-            self._data.append(element)
-            self._size += 1
-
-        
-        elif len(args) == 2:
-            index, element = args
-
-            if index < 0 or index > self._size:
-                raise IndexError("Índice fuera de rango")
-
-            self._data.append(None)
-
-            for i in range(self._size, index, -1):
-                self._data[i] = self._data[i - 1]
-
-            self._data[index] = element
-            self._size += 1
-
-        else:
-            raise TypeError("Número incorrecto de argumentos")
-
+    def peek(self):
+        if self.isEmpty():
+            raise IndexError("Peek desde una cola vacía")
+        return self.elements[0]
     
-    def remove(self, index):
-
-        if index < 0 or index >= self._size:
-            raise IndexError("Índice fuera de rango")
-
-        removed = self._data[index]
-
-        for i in range(index, self._size - 1):
-            self._data[i] = self._data[i + 1]
-
-        self._data.pop()
-        self._size -= 1
-
-        return removed
-
+    def _iter_(self):
+        return iter(self.elements)
     
-    def get(self, index):
-        if index < 0 or index >= self._size:
-            raise IndexError("Índice fuera de rango")
-        return self._data[index]
-
-    def set(self, index, element):
-        if index < 0 or index >= self._size:
-            raise IndexError("Índice fuera de rango")
-        self._data[index] = element
-
-    def contains(self, element):
-        for i in range(self._size):
-            if self._data[i] == element:
-                return True
-        return False
-
+    def _contains_(self, element):
+        return element in self.elements
+    
+    def push(self, element):
+        self.elements.append(element)
+    
+    def pop(self, index=0):
+        if self.isEmpty():
+            raise IndexError("Pop desde una cola vacía")
+        return self.elements.pop(index)
+    
     def clear(self):
-        self._data = []
-        self._size = 0
+        self.elements.clear()
+    
+    def extend(self, iterable):
+        self.elements.extend(iterable)
+    
+    def copy(self):
+        return Queue(self.elements.copy())
+    
+    def tail(self):
+        if self.isEmpty():
+            raise IndexError("Tail desde una cola vacía")
+        return self.elements[-1]
 
-
+// Código Circular List
 
 class Node:
-    def __init__(self, data):
+    def _init_(self, data):
         self.data = data
         self.next = None
 
 
-class LinkedList:
-
-    def __init__(self):
+class CircularList:
+    def _init_(self):
         self.head = None
-        self._size = 0
+        self.tail = None
 
-    def size(self):
-        return self._size
-
-    def isEmpty(self):
-        return self._size == 0
-
-    
-    def addFirst(self, element):
-        new_node = Node(element)
-        new_node.next = self.head
-        self.head = new_node
-        self._size += 1
-
-    def addLast(self, element):
-        new_node = Node(element)
+    def append(self, value):
+        new_node = Node(value)
 
         if self.head is None:
             self.head = new_node
+            self.tail = new_node
+            new_node.next = self.head
         else:
-            current = self.head
-            while current.next:
-                current = current.next
-            current.next = new_node
+            self.tail.next = new_node
+            self.tail = new_node
+            self.tail.next = self.head
 
-        self._size += 1
-
-    def add(self, index, element):
-
-        if index < 0 or index > self._size:
-            raise IndexError("Índice fuera de rango")
-
-        if index == 0:
-            self.addFirst(element)
+    def insert(self, index, value):
+        if index < 0:
             return
 
-        new_node = Node(element)
-        current = self.head
+        new_node = Node(value)
 
-        for _ in range(index - 1):
+        if index == 0:
+            if self.head is None:
+                self.head = self.tail = new_node
+                new_node.next = self.head
+            else:
+                new_node.next = self.head
+                self.head = new_node
+                self.tail.next = self.head
+            return
+
+        current = self.head
+        i = 0
+
+        while i < index - 1 and current.next != self.head:
             current = current.next
+            i += 1
 
         new_node.next = current.next
         current.next = new_node
-        self._size += 1
 
-    
-    def removeFirst(self):
-        if self.isEmpty():
-            raise IndexError("Lista vacía")
+        if current == self.tail:
+            self.tail = new_node
 
-        removed = self.head.data
-        self.head = self.head.next
-        self._size -= 1
-        return removed
-
-    def removeLast(self):
-        if self.isEmpty():
-            raise IndexError("Lista vacía")
-
-        if self._size == 1:
-            return self.removeFirst()
-
-        current = self.head
-        while current.next.next:
-            current = current.next
-
-        removed = current.next.data
-        current.next = None
-        self._size -= 1
-        return removed
-
-    def remove(self, index):
-
-        if index < 0 or index >= self._size:
-            raise IndexError("Índice fuera de rango")
+    def delete(self, index):
+        if self.head is None or index < 0:
+            return
 
         if index == 0:
-            return self.removeFirst()
+            if self.head == self.tail:
+                self.head = None
+                self.tail = None
+            else:
+                self.head = self.head.next
+                self.tail.next = self.head
+            return
 
         current = self.head
-        for _ in range(index - 1):
+        i = 0
+
+        while i < index - 1 and current.next != self.head:
             current = current.next
+            i += 1
 
-        removed = current.next.data
-        current.next = current.next.next
-        self._size -= 1
-        return removed
+        node_to_delete = current.next
+        current.next = node_to_delete.next
 
-    
-    def get(self, index):
-        if index < 0 or index >= self._size:
-            raise IndexError("Índice fuera de rango")
+        if node_to_delete == self.tail:
+            self.tail = current
 
-        current = self.head
-        for _ in range(index):
-            current = current.next
+    def display(self):
+        if self.head is None:
+            print("Lista vacía")
+            return
 
-        return current.data
+        temp = self.head
+        while True:
+            print(temp.data, end=" -> ")
+            temp = temp.next
+            if temp == self.head:
+                break
 
-    def set(self, index, element):
-        if index < 0 or index >= self._size:
-            raise IndexError("Índice fuera de rango")
-
-        current = self.head
-        for _ in range(index):
-            current = current.next
-
-        current.data = element
-
-    def contains(self, element):
-        current = self.head
-        while current:
-            if current.data == element:
-                return True
-            current = current.next
-        return False
-
-    def clear(self):
-        self.head = None
-        self._size = 0
+        print("(Head)")
 
 
+cl = CircularList()
 
+cl.append("A")
+cl.append("B")
+cl.append("C")
+cl.display()
 
-if __name__ == "__main__":
+cl.delete(0)
+cl.display()
 
-    print("PRUEBA ARRAYLIST")
-    arr = ArrayList()
-    arr.add(10)
-    arr.add(20)
-    arr.add(1, 99)
-    print("Elemento en índice 1:", arr.get(1))
-    print("Contiene 20:", arr.contains(20))
-    print("Eliminado:", arr.remove(1))
-    print("Tamaño:", arr.size())
-
-    print("\nPRUEBA LINKEDLIST")
-    lst = LinkedList()
-    lst.addFirst(5)
-    lst.addLast(15)
-    lst.add(1, 10)
-    print("Elemento en índice 1:", lst.get(1))
-    print("Contiene 15:", lst.contains(15))
-    print("Eliminado:", lst.remove(100))
-    print("Tamaño:", lst.size())
+cl.insert(1, "Z")
+cl.display()
